63459581ef8c9c05f5c7fc31deb647d2c8315dda
Administrator <893923042@qq.com>
2021-03-13 00:23:10 +0800

    basic building

--- merchant/merchant-web/src/main/java/com/skyline/merchant/MerchantApplication.java
+++ merchant/merchant-web/src/main/java/com/skyline/merchant/MerchantApplication.java
@@ -1,17 +1,13 @@
-package com.skyline.spider;
+package com.skyline.merchant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import zipkin.server.EnableZipkinServer;
 
-@EnableZipkinServer
 @EnableEurekaClient
 @SpringBootApplication
-public class SpiderApplication {
-
+public class MerchantApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpiderApplication.class, args);
+        SpringApplication.run(MerchantApplication.class, args);
     }
-
 }
---
+++ merchant/merchant-web/src/main/java/com/skyline/merchant/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.skyline.merchant.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/merchant")
+public class UserController {
+
+    @PostMapping("/echo")
+    public String echo(@RequestParam("var") String var){
+        return var;
+    }
+}
